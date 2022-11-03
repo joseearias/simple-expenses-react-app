f6651cfe56b13c4bd562642a02892019ba3f6bd2
José Arias <j.ariascanto@gmail.com>
2022-11-03 00:19:34 -0600

    created repository

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterYearHandler = (enteredYear) => {
+    setFilteredYear(enteredYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilteredYear={filterYearHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expenses;
